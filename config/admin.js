9e67219d7eb98a533f4daf7e2e8ea97270567de3
youngsterjaidev <47059807+youngsterjaidev@users.noreply.github.com>
2021-07-22 14:24:07 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,7 @@
 module.exports = {
 	// dashboard UI language
 	language: 'en',
-	apiBaseUrl: 'http://localhost:3001/api/v1',
-	apiWebSocketUrl: 'ws://localhost:3001',
+	apiBaseUrl: `http://localhost:${process.env.PORT}/api/v1`,
+	apiWebSocketUrl: `ws://localhost:${process.env.PORT}`,
 	developerMode: true
 };
